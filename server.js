dec1c41d865b5c2b6ecb917a465b9c6b22a47be0
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 20:55:26 +0200

    init the server

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-//app.use(express.static("build"));
 app.get("/", function(req, res) {
-  // res.sendFile(path.resolve(__dirname, "index.html"));
   res.send("HMMMMMMMM");
 });
 
