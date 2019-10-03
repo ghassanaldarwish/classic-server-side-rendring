3cd7bf1212f95ac4960c1675f640b754cef50eaf
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 20:50:42 +0200

    init the server

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static("build"));
+//app.use(express.static("build"));
 app.get("/", function(req, res) {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  // res.sendFile(path.resolve(__dirname, "index.html"));
+  res.send("HMMMMMMMM");
 });
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 5000);
 
 console.log("Running at Port 3000");
