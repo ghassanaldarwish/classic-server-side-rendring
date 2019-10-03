023537b49121a8d92f858175b6d7f7fd256fd472
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 22:45:25 +0200

    testing fs for horuko

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "test", "test.json"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, (req, res) => {
