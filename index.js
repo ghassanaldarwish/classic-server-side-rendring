491192950db36681497f92a6c8bb073124cb975f
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 21:17:15 +0200

    init the server

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(express.static(path.join(__dirname, "build")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
   app.get("*", (req, res) => {
-    res.send("HMMMMMMMMMMMMMMMMMMM");
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
 app.get("*", (req, res) => {
-  res.send("HMMMMMMMMMMMMMMMMMMM");
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 app.listen(port, (req, res) => {
