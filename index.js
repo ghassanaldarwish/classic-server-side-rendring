a221047a831124f081be8f4aa64630ab51912f3e
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 21:09:26 +0200

    init the server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "build/index.html")));
+  });
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "build/index.html"));
+});
+
+app.listen(port, (req, res) => {
+  console.log(`server listening on port: ${port}`);
+});
