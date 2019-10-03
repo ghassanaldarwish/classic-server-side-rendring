f9f5633f6a6402050752d8c3b346f4402c466b3d
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 20:38:36 +0200

    init the server

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const router = express.Router();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(express.static("build"));
-router.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
-app.use("*", router);
+
 app.listen(process.env.port || 3000);
 
 console.log("Running at Port 3000");
