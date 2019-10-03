8e31afc0990738c6a46668fd439beae90a61bd8d
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2019-10-03 20:23:27 +0200

    init the server

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 const path = require("path");
 const router = express.Router();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("build"));
 router.get("*", function(req, res) {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
